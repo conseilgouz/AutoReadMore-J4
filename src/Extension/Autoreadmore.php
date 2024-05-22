a7c4a76c3ceb7e8af144d3151dbfcfc4c8640982
Pascal Leconte <pascal.leconte@conseilgouz.com>
2024-05-22 10:45:54 +0200

    5.2.7
    Add merge paragraph option if existing readmore are to be ignored

--- src/Extension/Autoreadmore.php
+++ src/Extension/Autoreadmore.php
@@ -171,7 +171,11 @@ final class Autoreadmore extends CMSPlugin implements SubscriberInterface
         $this->fulltext_loaded = false;
 
         if ($this->params->get('Ignore_Existing_Read_More') && isset($article->introtext) && isset($article->fulltext)) {
-            $text = $article->introtext . PHP_EOL . $article->fulltext;
+            if ($this->params->get('Merge_After_Ignore', 0)) {// merge intro + fulltext => ignore last </p> intro, first <p> fulltext 
+                $text = preg_replace('/<\/.*$/', '', $article->introtext).preg_replace('/<p[^>]*>/', '', $article->fulltext, 1);
+            } else {
+                $text = $article->introtext . PHP_EOL . $article->fulltext;
+            }
             if (file_exists(JPATH_PLUGINS ."/content/cck")) { //check if Seblod is installed
                 $text = preg_replace('/::cck::(\d+)::\/cck::/', '', $text) ; //remove the ::cck:: tags
                 $text = preg_replace('/::introtext::/', '', $text) ; //remove the ::introtext:: tags, keep content
